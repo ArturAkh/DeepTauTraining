fdb2ac3b1688f08f01b62671055c8e6507f5e1ba
Artur Gottmann <artur.gottmann@desy.de>
2021-04-07 09:38:30 +0200

    Fix date output format in shell scripts

--- balanced_batches/run_jobs.sh
+++ balanced_batches/run_jobs.sh
@@ -8,13 +8,13 @@ OUTPUT=batch${JOBID}.root
 
 source additional_environment.sh
 
-echo "Performing job with configuration ${JOBCONFIG} with job ID ${JOBID}" $(date +"%Y-%M-%d %H:%M:%S.%6N")
+echo "Performing job with configuration ${JOBCONFIG} with job ID ${JOBID}" $(date +"%Y-%m-%d %H:%M:%S.%6N")
 
 ./run_batches.sh ${JOBCONFIG} ${JOBID}
 
-echo "Copying output" $(date +"%Y-%M-%d %H:%M:%S.%6N")
+echo "Copying output" $(date +"%Y-%m-%d %H:%M:%S.%6N")
 echo "Destination: ${BALANCED_BATCHES_OUTPUTDIR}"
 
 ./copy_batches.sh ${OUTPUT} ${BALANCED_BATCHES_OUTPUTDIR}
 
-echo "Job finished" $(date +"%Y-%M-%d %H:%M:%S.%6N")
+echo "Job finished" $(date +"%Y-%m-%d %H:%M:%S.%6N")
